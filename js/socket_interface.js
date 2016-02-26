8fbc5eac8c2bd708179f15f7b6af438a317590be
Ashvala Vinay <mail@ashvala.net>
2016-02-26 18:45:48 -0500

    some changes.
    - WebMIDI API work begins here.
    
    - Mongoose is still an annoyance to work with.
    
    - Start debating whether you need a separate client on the client-side
    to do MIDI or just do it through browser.

--- js/socket_interface.js
+++ js/socket_interface.js
@@ -1,5 +1,4 @@
 var socket = io.connect("http://crimson.local:8181");
-<<<<<<< HEAD
 //Me. Just me.
 var me = {};
 // Get instrument number
@@ -283,35 +282,6 @@ socket.on("client_add", function(obj) {
 
 socket.on("serve_choices", function() {
     socket.emit("request_orc")
-=======
-
-socket.on('connect', function(){
-  socket.emit('get_instruments');
-})
-
-var instruments;
-var new_id;
-
-function parse_instruments(arr){
-  arr.forEach(function(err, arr_val){
-    console.log(arr[arr_val].name);
-    var div_str = "<div class='instr_list_button' data-orc='" + arr[arr_val].orchestra + "' data-type='existing_ins' data-id='" + arr[arr_val]._id + "' data-name='" + arr[arr_val].name + "'>" + arr[arr_val].name + "</div>"
-    $(".instr_list").append(div_str)
-  });
-}
-
-socket.on('instrs', function(obj){
-  instruments = obj;
-  parse_instruments(instruments)
-});
-
-socket.on('instr_new', function(obj){
-  new_id = obj
-  $(".editor").val("")
-  var div_str = "<div class='instr_list_button' data-orc='' data-type='new_ins' data-id='" + new_id + "'> new instrument" + "</div>"
-  $(".instr_list").append(div_str)
-  $(".file_name").val("new instrument")
->>>>>>> origin/master
 });
 
 // aw, data about me? AWWWW
