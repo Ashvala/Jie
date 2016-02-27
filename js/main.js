7cd7d510ade18083bc18f8b2d7c03eb6835124ff
Ashvala Vinay <mail@ashvala.net>
2016-02-26 22:33:16 -0500

    fix boolean

--- js/main.js
+++ js/main.js
@@ -263,7 +263,7 @@ $(document).ready(function() {
     ]
 
     $(document).on("click", ".instrument_button", function() {
-        if ($(this).attr("data-disabled") == "false") {
+        if ($(this).attr("data-disabled") == "false" && controlling_bool == false) {
             controlling_bool = true
             sectionNumber = parseInt($(this).attr("data-section-number"))
             temp_sec_val = split_orcs[sectionNumber]
