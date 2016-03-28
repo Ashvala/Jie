2234215b01926c8bbc26335ba8eb69b793ddd447
Ashvala Vinay <mail@ashvala.net>
2016-03-28 00:07:56 -0400

    synch up animations and clean up csound code.

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,4 @@
+var curr_beat_on_animate = 0
 glow_animate = function(div_obj) {
     original_lightness = $.Color(div_obj, 'background').lightness()
     new_lightness = original_lightness + 0.1;
@@ -31,12 +32,9 @@ glow_animate_svg = function(div_obj) {
     new_hsla.h = original_hsla.hue
     new_hsla.s = original_hsla.saturation
     new_hsla.l = new_lightness
-    console.log(original_hsla[0]);
     var g = main_circle.select("circle")
         //    console.log(g)
-    console.log("Hue: " + original_hsla[0])
-    console.log("Saturation: " + original_hsla[1])
-    console.log("Lightness: " + original_hsla[2])
+
     var glow1 = function() {
         g.animate({
             fill: Snap.hsl((original_hsla[0]), (original_hsla[1] * 100), ((original_hsla[2] + 0.1) * 100))
@@ -83,19 +81,20 @@ glow_repeats = function() {
         } else {
             beat_str = "[data-beat=" + beat_val + "]"
         }
+        curr_beat_on_animate = beat_val
         $(beat_str).transition({
             "-webkit-filter": "brightness(0.8)"
         }).delay(10).transition({
             "-webkit-filter": "brightness(1)"
         })
         n += 1
-    }, 240)
+    }, 250)
 }
 
 scale_svg = function(item) {
     var scaler = Snap(item);
     var s = scaler.select("path")
-    console.log(s)
+
     var mat_scaler_lower = new Snap.Matrix()
     mat_scaler_lower.scale(0.99, 1.0)
     var mat_scaler_higher = new Snap.Matrix()
@@ -112,7 +111,7 @@ scale_svg = function(item) {
 scale_svg_release = function(item, scale_val){
     var scaler = Snap(item);
     var s = scaler.select("path")
-    console.log(s)
+
     var mat_scaler_lower = new Snap.Matrix()
     mat_scaler_lower.scale(scale_val, 1.0)
     var animate_1 = function(){
